b0f2335e71e6b69f1f27ea512180c55b271a0939
Weronika <weronika.gabriela.przybyla@gmail.com>
2023-01-21 14:34:37 +0100

    Calculator

--- src/main/java/com/kodilla/CalculatorProgram.java
+++ src/main/java/com/kodilla/CalculatorProgram.java
@@ -1,7 +1,27 @@
 package com.kodilla;
 
-public class Main {
+public class CalculatorProgram {
+
+    int a;
+    int b;
+
+    public int addition(int a, int b) {
+        return a+b;
+    }
+
+    public int subtraction(int a, int b) {
+        return a-b;
+    }
+
+    public int multiplication(int a, int b) {
+        return a*b;
+    }
+
+    public int division(int a, int b) {
+        return a/b;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CalculatorProgram calculator = new CalculatorProgram();
+        System.out.println("Result is " + calculator.addition(7,3));
     }
 }
